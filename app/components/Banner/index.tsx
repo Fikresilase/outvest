183ba8680cb6d3729e72d13c406a10317822ad4e
Fikresilase <shitubilbila@gmail.com>
2024-09-14 17:23:58 +0300

    banner section compete and video added

--- app/components/Banner/index.tsx
+++ app/components/Banner/index.tsx
@@ -11,21 +11,20 @@ const Banner = () => {
         <div className='bg-image relative' id="home-section">
             <div className='arrowOne'></div>
             <div className='radial-banner hidden lg:block'></div>
-            <ModalVideo channel='youtube' isOpen={isOpen} videoId="1YyAzVmP9xQ" onClose={() => setOpen(false)} />
-
+            <ModalVideo channel='youtube' isOpen={isOpen} videoId="Z3D6pGc7nHw" onClose={() => setOpen(false)} />
             <div className="mx-auto max-w-7xl pt-16 lg:pt-40 sm:pb-24 px-6">
 
                 <div className='height-work'>
                     <div className='grid grid-cols-1 lg:grid-cols-12 my-16'>
                         <div className='arrowTwo'></div>
                         <div className='col-span-7'>
-                            <h1 className="text-4xl lg:text-7xl font-bold mb-5 text-white md:4px md:text-start text-center">
-                                Buy,  Sell & Accept <br /> Digital Assets
+                            <h1 className="text-4xl lg:text-5xl font-bold mb-5 text-white md:4px md:text-start text-center">
+                                The new way of portfolio <br /> Diversification
                             </h1>
-                            <p className='text-white md:text-lg font-normal mb-10 md:text-start text-center'>Lorem Ipsum is simply dummy text of the printing and <br /> typesetting industry. Lorem Ipsum has been the industry <br /> standard dummy text ever since the 1500s</p>
+                            <p className='text-white md:text-lg font-normal mb-10 md:text-start text-center'>Politicians are some of the best investors. Diversify <br/>your portfolio by aligning with the investment strategies <br /> of top lawmakers to boost returns and minimize risk.</p>
                             <div className='flex align-middle justify-center md:justify-start'>
                                 <button className='text-xl font-semibold text-white py-4 px-6 lg:px-12 navbutton mr-6'>Get Started</button>
-                                <button onClick={() => setOpen(true)} className='bg-transparent flex justify-center items-center text-white'><Image src={'/images/Banner/playbutton.svg'} alt="button-image" className='mr-3' width={47} height={47} />How it work</button>
+                                <button onClick={() => setOpen(true)} className='bg-transparent flex justify-center items-center text-white'><Image src={'/images/Banner/playbutton.svg'} alt="button-image" className='mr-3' width={47} height={47} />How politicians</button>
                             </div>
                         </div>
 
